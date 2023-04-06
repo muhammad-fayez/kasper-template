b98bb905d0506d7cc8178cdddab1f2a6884bc196
muhammad-fayez <muhammad20847@feng.bu.edu.eg>
2023-04-06 14:24:34 +0200

    new template

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+document.querySelector(".bar-icon").addEventListener("click", function () {
+    if (document.querySelector(".navbar").style.display === "block") {
+        document.querySelector(".navbar").style.display = "none"
+    } else {
+        document.querySelector(".navbar").style.display = "block"
+
+    }
+})
+
+
+
+
+
+
+
+
+
